5549bfc1433b480959c488a37a5d2f1233e5728a
xiannvrong <1796940658@qq.com>
2019-03-04 18:28:26 +0800

    意见反馈

---
+++ pages/user/feedback/feedback.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="feedback_wrapper">
+		<textarea value="" placeholder="请输入您的宝贵意见" placeholder-style="color:#999;"/>
+		<button type="primary" class="submit_btn">提交</button>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	components: {},
+	computed: {},
+	methods: {}
+};
+</script>
+
+<style lang="less" scoped>
+.feedback_wrapper {
+	width: 100%;
+	padding: 24upx;
+	box-sizing: border-box;
+	.submit_btn {
+		width: 100%;
+		height: 88upx;
+		background-color: #7ca7d2;
+		border-radius: 6upx;
+		color: #ffffff;
+		font-size: 28upx;
+		text-align: center;
+		line-height: 88upx;
+		margin-top: 148upx;
+	}
+}
+.feedback_wrapper > textarea {
+	width: 702px;
+	height: 428px;
+	background: rgba(255, 255, 255, 1);
+	border-radius: 6px;
+	border: 1px solid rgba(236, 236, 236, 1);
+	font-size: 24px;
+	color: #999999;
+	padding: 24upx;
+	box-sizing: border-box;
+}
+</style>
--- pages/user/user.vue
+++ pages/user/user.vue
@@ -21,7 +21,7 @@
 			<uni-list-item title="我的收藏" thumb="http://qnimage.xiteng.com/shoucang@2x.png"></uni-list-item>
 			
 			<uni-list-item title="我的积分" thumb="http://qnimage.xiteng.com/jifen@2x.png"></uni-list-item>
-			<uni-list-item title="意见反馈" thumb="http://qnimage.xiteng.com/fankui@2x.png"></uni-list-item>
+			<uni-list-item title="意见反馈" thumb="http://qnimage.xiteng.com/fankui@2x.png" @click="feedback(feedback)"></uni-list-item>
 			<uni-list-item title="更多" thumb="http://qnimage.xiteng.com/gengduo@2x.png"></uni-list-item>
 		</uni-list>
 		<view class="btn-row">
@@ -82,6 +82,11 @@
 					url:"personalData/personalData"
 				})
 			},
+			feedback(feedback){
+				uni.navigateTo({
+					url:"feedback/feedback"
+				})
+			}
 		}
 	}
 </script>
