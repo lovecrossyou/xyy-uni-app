5f40dcfa58271ab03d6082038c31f50b3d30cc55
lovecross <lovecrossyoutt@hotmail.com>
2019-04-17 21:59:04 +0800

    xx

--- util/request.js
+++ util/request.js
@@ -1,9 +1,9 @@
 import Fly from 'flyio/dist/npm/wx'
 import service from "../service"
 const request = new Fly()
-export const baseURL = 'http://47.94.169.143:8004/'
+// export const baseURL = 'http://47.94.169.143:8004/'
 // export const baseURL = 'http://192.168.1.235:8004/'
-// export const baseURL = 'http://192.168.199.101:8004/'
+export const baseURL = 'http://192.168.199.101:8004/'
 
 request.config.baseURL = baseURL
 
