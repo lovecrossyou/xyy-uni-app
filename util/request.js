32adc3de14d582d658147ad02b37d0994725da97
lovecross <lovecrossyoutt@hotmail.com>
2019-04-19 00:19:21 +0800

    xx

--- util/request.js
+++ util/request.js
@@ -2,7 +2,9 @@ import Fly from 'flyio/dist/npm/wx'
 import service from "../service"
 const request = new Fly()
 
-export const baseURL = 'http://47.94.169.143:8004/'
+// export const baseURL = 'http://47.94.169.143:8004/'
+export const baseURL = 'https://api.kuaimayoupin.com/'
+
 // export const baseURL = 'http://192.168.1.235:8001/'
 // export const baseURL = 'http://192.168.199.101:8004/'
 
